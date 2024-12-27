46733f71d53c2abccf2fc7adbfdc236cb38af83f
Rajesh Kumar <rajeshiitbhu2@gmail.com>
2024-12-27 21:35:29 +0400

    added loader

--- app/components/Loader.js
+++ app/components/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50 ">
       <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
     </div>
   );
